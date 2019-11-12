0ea871cd1bb1f04d164c8043480e2c7ecf14b34c
123name123 <46817994+123name123@users.noreply.github.com>
2019-11-12 16:40:02 +0300

    Update rogram.py

--- rogram.py
+++ rogram.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('UFO came and added this line')
+    print('Edit in github')
 
 
 if __name__ == '__main__':
